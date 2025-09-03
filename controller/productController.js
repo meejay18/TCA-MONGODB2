d5023dc6f572e911bbaeff38cc2043a4016f2886
digbanmije18@gmail.com <digbanmije18@gmail.com>
2025-09-03 23:24:32 +0100

    updates

--- controller/productController.js
+++ controller/productController.js
@@ -67,3 +67,51 @@ exports.getAllProducts = async (req, res) => {
     })
   }
 }
+
+exports.updateProduct = async (req, res) => {
+  const { productId } = req.params
+  const { productName, price, quantity, description } = req.body
+  try {
+    const product = await productModel.findById(productId)
+    if (!product) {
+      res.status(404).json({
+        message: `No product found`,
+      })
+    }
+
+    const data = { productName, price, quantity, description }
+    const updatedProduct = await productModel.findByIdAndUpdate(productId, data, { new: true })
+    return res.status(200).json({
+      message: 'Product updated',
+      data: updatedProduct,
+    })
+  } catch (error) {
+    return res.status(500).json({
+      message: 'Error creating product',
+      error: error.message,
+    })
+  }
+}
+
+exports.deleteProduct = async (req, res) => {
+  const { productId } = req.params
+  try {
+    const product = await productModel.findById(productId)
+    if (!product) {
+      res.status(404).json({
+        message: `No product found`,
+      })
+    }
+
+    const deletedProduct = await productModel.findByIdAndDelete(productId)
+    return res.status(200).json({
+      message: 'Product deleted',
+      data: deletedProduct,
+    })
+  } catch (error) {
+    return res.status(500).json({
+      message: 'Error deleting product',
+      error: error.message,
+    })
+  }
+}
--- route/productRoute.js
+++ route/productRoute.js
@@ -1,9 +1,17 @@
 const express = require('express')
-const { createProduct, getOneProduct, getAllProducts } = require('../controller/productController')
+const {
+  createProduct,
+  getOneProduct,
+  getAllProducts,
+  updateProduct,
+  deleteProduct,
+} = require('../controller/productController')
 const route = express.Router()
 
 route.post('/product', createProduct)
 route.get('/product/:productId', getOneProduct)
 route.get('/product', getAllProducts)
+route.put('/product/:productId', updateProduct)
+route.delete('/product/:productId', deleteProduct)
 
 module.exports = route
